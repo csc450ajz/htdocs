0144b0023ee75824ca83a8f1a485d39e21dc4eaa
acmaser12 <49576923+acmaser12@users.noreply.github.com>
2020-02-12 21:06:47 -0600

    Created basic navbar features; dropdown and search

--- index.php
+++ index.php
@@ -11,14 +11,10 @@
     <div class="container">
         <div class="row justify-content-sm-center">
             <div class="col col-sm-10">
-                <h1 style="margin-top: 30px;">This is a test of the PHP setup</h1>
+                <h1 style="margin-top: 30px;">This is where the content will go</h1>
             </div>
         </div>
     </div>
 
-     <!-- jQuery CDN links for Bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
