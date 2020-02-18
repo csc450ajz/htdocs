7bb7a6578eee26d79a8f0bc9ca4d272eead4c19c
zulkiflmohammed <zulkiflmohammed@gmail.com>
2020-02-17 21:08:50 -0600

    Added image slider and featured products on home page

--- index.php
+++ index.php
@@ -1,20 +1,112 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-        require_once('config.html')
+require_once('config.html')
 ?>
+
 <head>
     <title>index.php</title>
 </head>
-<body>
 
-    <div class="container">
-        <div class="row justify-content-sm-center">
-            <div class="col col-sm-10">
-                <h1 style="margin-top: 30px;">This is to display the navbar so far</h1>
+<body>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="images/1.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="images/2.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="images/3.jpg" alt="Third slide">
+                </div>
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
 
+    <div class="container">
+        <h2 class="text-center">Featured Products</h2>
+
+        <div class="row">
+
+            <div class="col-sm-3">
+
+                <div class="card">
+                    <img src="images/1.jpg" alt="" style="width: auto; height: 200px;">
+                    <div class="card-body">
+                        <h4 class="card-title"><b>Basketball</b></h4>
+                        <p class="card-text">Price: $20.00</p>
+                        <a href="#" class="btn btn-primary">Add to Order</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-sm-3">
+
+                <div class="card">
+                    <img src="images/1.jpg" alt="" style="width: auto; height: 200px;">
+                    <div class="card-body">
+                        <h4 class="card-title"><b>Basketball</b></h4>
+                        <p class="card-text">Price: $20.00</p>
+                        <a href="#" class="btn btn-primary">Add to Order</a>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-sm-3">
+
+                <div class="card">
+                    <img src="images/1.jpg" alt="" style="width: auto; height: 200px;">
+                    <div class="card-body">
+                        <h4 class="card-title"><b>Basketball</b></h4>
+                        <p class="card-text">Price: $20.00</p>
+                        <a href="#" class="btn btn-primary">Add to Order</a>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-sm-3">
+
+                <div class="card">
+                    <img src="images/1.jpg" alt="" style="width: auto; height: 200px;">
+                    <div class="card-body">
+                        <h4 class="card-title"><b>Basketball</b></h4>
+                        <p class="card-text">Price: $20.00</p>
+                        <a href="#" class="btn btn-primary">Add to Order</a>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+        
+    </div>
+
+
+
 </body>
+
 </html>
