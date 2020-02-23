3fdb5f74da4817eefb1cd5608861c2a75ece44a2
zulkiflmohammed <zulkiflmohammed@gmail.com>
2020-02-22 20:26:52 -0600

    Added admin home page, hard coded data, p

---
+++ admin.php
@@ -0,0 +1,96 @@
+<!-- logIn.php - login page for user's
+      
+    -->
+
+
+
+<!DOCTYPE html>
+<html>
+<?php
+require_once('config.html')
+?>
+
+<head>
+    <title>Admin</title>
+</head>
+
+<body>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h3 class="display-4">Welcome Admin</h3>
+            <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+                    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Messages</a>
+                    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Users</a>
+                    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Categories</a>
+                    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Coupons</a>
+
+                </div>
+            </nav>
+
+            <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+
+                    <br>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="card border-dark mb-3 " style="margin-bottom: unset!important;">
+                                <div class="card-header">Quick Profile Info</div>
+                                <img class="card-img-top" src="images/Logo.png" alt="Card image cap">
+                                <div class="card-body text-dark">
+                                    <h5 class="card-title">My Name</h5>
+                                    <p class="card-text">User Details Below: email, username, etc.</p>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-9">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="card">
+                                        <div class="card-header"><strong>Total Users</strong></div>
+                                        <div class="card-body" align="center">
+                                            <h1>100</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="card">
+                                        <div class="card-header"><strong>Total Categories</strong></div>
+                                        <div class="card-body" align="center">
+                                            <h1>10</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="card">
+                                        <div class="card-header"><strong>Unread Messages</strong></div>
+                                        <div class="card-body" align="center">
+                                            <h1>15</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="card">
+                                        <div class="card-header"><strong>Coupons</strong></div>
+                                        <div class="card-body" align="center">
+                                            <h1>8</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+                <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
