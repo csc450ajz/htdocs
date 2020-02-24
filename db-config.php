a2861b9e6c76cac592f506248c7480c46dbe99b8
acmaser12 <49576923+acmaser12@users.noreply.github.com>
2020-02-23 21:29:42 -0600

    Commented out debug code in file.

--- db-config.php
+++ db-config.php
@@ -17,15 +17,15 @@
     $conn = new mysqli(SERVER_NAME, DBF_USER_NAME, DBF_PASSWORD);
     $conn->select_db(DATABASE_NAME);
     
-    // test the connection (TODO -- Remove later after functioning)
-    $sql = "SELECT * FROM User";
-    $result = $conn -> query($sql);
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "User Email: " . $row["userEmail"];
-        }
-    } else {
-        echo "0 results";
-    }
+    // DEBUG test the connection
+    //$sql = "SELECT * FROM User";
+    //$result = $conn -> query($sql);
+    //if ($result->num_rows > 0) {
+    //    // output data of each row
+    //    while($row = $result->fetch_assoc()) {
+    //        echo "User Email: " . $row["userEmail"];
+    //    }
+    //} else {
+    //    echo "0 results";
+    //}
 ?>
