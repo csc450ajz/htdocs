9b2084ca5c3c5d6a5408280373d3e1ffcb6b9f25
acmaser12 <49576923+acmaser12@users.noreply.github.com>
2020-02-25 20:04:30 -0600

    Added file to check for users being logged in and their type.

---
+++ check-login.php
@@ -0,0 +1,61 @@
+<?php 
+    /*
+    This page checks that a user is logged in and
+    either allows them to continue the session or 
+    kicks them back to the login page.
+
+    Developer: Please require_once() this on any page 
+    requiring a user to be logged in. Then, call the 
+    particular function that is needed for the page.
+    */
+
+    /* 
+    ---------------
+    checkLogin()
+    ---------------
+    - Checks if user is logged in. 
+    - Returns true if logged in, false if not logged in.
+    NOTE: This only checks that a user is logged in,
+    not what type of user they are
+    */
+    function checkLogin() {
+        if (isset($_SESSION['login'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /* 
+    ---------------
+    checkAdmin()
+    ---------------
+    - Checks if user is logged in. 
+    - Checks if user is admin type.
+    - Returns true if logged in and user type is admin, false if not both.
+    */
+    function checkAdmin() {
+        if (checkLogin() && $_SESSION['login'] = "admin") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /* 
+    ---------------
+    checkClient()
+    ---------------
+    - Checks if user is logged in. 
+    - Checks if user is client type.
+    - Returns true if logged in and user type is type, false if not both.
+    */
+    function checkClient() {
+        if (checkLogin() && $_SESSION['login'] = "client") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+?>
