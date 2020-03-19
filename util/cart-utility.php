5d4e07a452dedb264ace1e46930e2b07952373dc
acmaser12 <49576923+acmaser12@users.noreply.github.com>
2020-03-19 17:46:19 -0500

    Updating live site db debugging.

--- util/cart-utility.php
+++ util/cart-utility.php
@@ -27,7 +27,7 @@
             // execute stored procedure
             $sql = "CALL insertCartItem('$productId', '$userEmail');";
             $cartResult = $conn->query($sql);
-            //$conn->next_result(); // allows following queries to occur
+            $conn->next_result(); // allows following queries to occur
             echo $conn->error;
         } else {
             header("Location: /logIn.php");
@@ -50,7 +50,7 @@
             // execute stored procedure
             $sql = "CALL getCartItems('$userEmail');";
             $result = $conn->query($sql);
-            $conn->next_result();
+            //$conn->next_result();
             echo $conn->error;
             return $result;
         }
