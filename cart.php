ddda12603c730f646de90439dadc411154e83a48
zulkiflmohammed <zulkiflmohammed@gmail.com>
2020-03-18 19:09:30 -0500

    Initial cart page

---
+++ cart.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include('databaseConnection.php');
+// include('util/db-config.php');
+
+require_once('util/config.html');
+
+$sql = "SELECT * FROM cartitems WHERE userEmail = 'admin@admin.com'";
+$result = $conn->query($sql);
+// $cartItems = mysqli_fetch_assoc($result);
+// echo json_encode($result -> fetch_all());
+// echo "<br />";
+
+
+?>
+
+<head>
+    <title>cart.php</title>
+</head>
+
+<body>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h2>Shopping Cart</h2>
+            <hr />
+
+            <div class="row">
+                <div class="col-md-8">
+                    <?php
+                    $total_price = 0;
+                    while ($cartItems = mysqli_fetch_assoc($result)) {
+                        // echo $cartItems['productId'];
+                        $sql = "SELECT * FROM product WHERE productId= " . $cartItems['productId'];
+                        $result2 = $conn->query($sql);
+                        // $product = mysqli_fetch_assoc($result);
+                        // <?php while ($product = mysqli_fetch_assoc($result)) : 
+
+                        // echo json_encode($product);
+
+                        while ($product = mysqli_fetch_assoc($result2)) {
+                            $total_price += ($product['productPrice']);
+                    ?>
+
+                            <div class="col-md-8">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <img src="images/balls.jpg" alt="" class="img-fluid img-thumbnail">
+                                            </div>
+                                            <div class="col-md-6">
+                                                <h3><?= $product['productName'] ?></h3>
+                                                <h4>$<?= $product['productPrice'] ?></h4>
+                                                <p><i>Sold by: Zulkifl M</i></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr />
+                            </div>
+                    <?php
+                        }
+                    }
+                    ?>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">Summary</div>
+                        <div class="card-body">
+                            <p class="card-text">TOTAL <span style="float: right;"><strong><?php echo "$ " . number_format($total_price, 2); ?></span> </p>
+                            <button class="btn btn-success">Place Order</button>
+                            <button class="btn btn-danger" style="float: right;">Cancel</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+    <?php
+    require_once('footer.html');
+
+    ?>
+
+</body>
+
+</html>
