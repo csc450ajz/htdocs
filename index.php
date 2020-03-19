eb2722e7498e7a0c0cc8bf706dd63b83d77e3fce
zulkiflmohammed <zulkiflmohammed@gmail.com>
2020-03-18 19:09:58 -0500

    Connected product to product detail page

--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-// include('databaseConnection.php');
-include('util/db-config.php');
+include('databaseConnection.php');
+// include('util/db-config.php');
 
 require_once('util/config.html');
+if (isset($_POST['productId'])) {
+    echo ($_POST['productId']);
 
+    switch ($_POST['btnCart']) {
+        case 'new':
+            $productId = $_POST['productId'];
+            $userEmail = "admin@admin.com";
+            $sql = "INSERT INTO cartitems (userEmail, productId) VALUES (?, ?) ";
+
+            // Set up a prepared statement
+            if ($stmt = $conn->prepare($sql)) {
+                // Pass the parameters
+                $stmt->bind_param("si", $userEmail, $productId);
+                if ($stmt->errno) {
+                    echo ("stmt prepare() had error.");
+                }
+
+                // Execute the query
+                $stmt->execute();
+                if ($stmt->errno) {
+                    echo ("Could not execute prepared statement");
+                }
+
+                // Store the result
+                $stmt->store_result();
+                $totalCount = $stmt->num_rows;
+
+                // Free results
+                $stmt->free_result();
+                // Close the statement
+                $stmt->close();
+            } // end if( prepare( ))
+
+            break;
+    }
+}
 
 ?>
 
@@ -50,130 +85,98 @@ require_once('util/config.html');
     </div>
 
     <div class="container">
-    <?php 
-         $sql = "SELECT * FROM product LIMIT 4";
-         $result = $conn->query($sql);
-    ?>
+        <?php
+        $sql = "SELECT * FROM product LIMIT 4";
+        $result = $conn->query($sql);
+        ?>
         <h2 class="text-center">Featured Products</h2>
 
         <div class="row">
-            
-            <?php while($product = mysqli_fetch_assoc($result)): ?>
-            <div class="col-sm-3">
 
-                <div class="card">
-                    <img src="images/1.jpg" alt="" style="width: auto; height: 200px;">
-                    <div class="card-body">
-                        <h4 class="card-title"><b><?= $product['productName']; ?></b></h4>
-                        <!-- TODO: Add product star reviews -->
-                        <p class="card-text"><?= $product['productPrice']; ?></p> 
-                        <!-- Send product id as encoded value -->
-                        <a href="productDetail.php?productId=<?php echo $product['productId'];?>" class="btn btn-primary">Details</a>
-                    </div>
+            <?php while ($product = mysqli_fetch_assoc($result)) : ?>
+
+                <div class="col-sm-3">
+                    <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+
+                        <div class="card">
+                            <img src="images/1.jpg" alt="" style="width: auto; height: 200px;">
+                            <div class="card-body">
+                                <h4 class="card-title"><b><a href="productDetail.php?productId=<?php echo $product['productId']; ?>"><?= $product['productName']; ?></a></b></h4>
+                                <!-- TODO: Add product star reviews -->
+                                <p class="card-text">$<?= $product['productPrice']; ?></p>
+                                <!-- Send product id as encoded value -->
+                                <input type="hidden" name="productId" value="<?= $product['productId']; ?>" />
+
+                                <button name="btnCart" value="new" class="btn btn-primary">Add to Cart</button>
+
+                            </div>
+                        </div>
+                    </form>
+
                 </div>
 
-            </div>
             <?php endwhile; ?>
-            <!-- <div class="col-sm-3">
-
-                <div class="card">
-                    <img src="images/1.jpg" alt="" style="width: auto; height: 200px;">
-                    <div class="card-body">
-                        <h4 class="card-title"><b>Basketball</b></h4>
-                        <p class="card-text">Price: $20.00</p>
-                        <a href="#" class="btn btn-primary">Add to Order</a>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="col-sm-3">
-
-                <div class="card">
-                    <img src="images/1.jpg" alt="" style="width: auto; height: 200px;">
-                    <div class="card-body">
-                        <h4 class="card-title"><b>Basketball</b></h4>
-                        <p class="card-text">Price: $20.00</p>
-                        <a href="#" class="btn btn-primary">Add to Order</a>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="col-sm-3">
-
-                <div class="card">
-                    <img src="images/1.jpg" alt="" style="width: auto; height: 200px;">
-                    <div class="card-body">
-                        <h4 class="card-title"><b>Basketball</b></h4>
-                        <p class="card-text">Price: $20.00</p>
-                        <a href="#" class="btn btn-primary">Add to Order</a>
-                    </div>
-                </div>
-
-            </div> -->
-
 
         </div>
 
 
         <hr>
 
-        
+
         <hr>
 
         <div class="row flex-row flex-nowrap overflow-auto">
             <div class="col-3">
                 <div class="card card-block">
-                <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
+                    <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
 
                 </div>
             </div>
             <div class="col-3">
                 <div class="card card-block">
-                <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
+                    <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
 
                 </div>
             </div>
             <div class="col-3">
                 <div class="card card-block">
-                <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
+                    <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
 
                 </div>
             </div>
             <div class="col-3">
                 <div class="card card-block">
-                <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
+                    <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
 
                 </div>
             </div>
 
             <div class="col-3">
                 <div class="card card-block">
-                <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
+                    <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
 
                 </div>
             </div>
             <div class="col-3">
                 <div class="card card-block">
-                <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
+                    <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
 
                 </div>
             </div>
             <div class="col-3">
                 <div class="card card-block">
-                <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
+                    <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
                 </div>
             </div>
             <div class="col-3">
                 <div class="card card-block">
-                <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
+                    <img class="d-block w-100" src="images/balls.jpg" alt="Balls">
 
                 </div>
             </div>
 
-            
-            
+
+
         </div>
 
     </div>
@@ -188,7 +191,8 @@ require_once('util/config.html');
 
 
 
-
+    <?php require_once('footer.html');
+    ?>
 </body>
 
 </html>
