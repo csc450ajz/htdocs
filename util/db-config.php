e6851543faf7d90789cdcd06f9d3397918165f4f
acmaser12 <49576923+acmaser12@users.noreply.github.com>
2020-03-29 20:26:07 -0500

    Updated to new db password.

--- util/db-config.php
+++ util/db-config.php
@@ -10,7 +10,7 @@
     // define credentials for db
     define("SERVER_NAME","mi3-ss48");
     define("DBF_USER_NAME", "sportrad_developer");
-    define("DBF_PASSWORD", "L0aHxAW66vgw");
+    define("DBF_PASSWORD", "Zn2IwDW98ek1");
     define("DATABASE_NAME", "sportrad_sportrader");
 
 	// establish connection
