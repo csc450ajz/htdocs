cf70d87d04793765331ade692c43da61a4cacfc4
acmaser12 <49576923+acmaser12@users.noreply.github.com>
2020-04-02 22:19:30 -0500

    Updated back to live creds.

--- util/db-config.php
+++ util/db-config.php
@@ -8,9 +8,9 @@
     */
 
     // define credentials for db
-    define("SERVER_NAME","localhost");
-    define("DBF_USER_NAME", "root");
-    define("DBF_PASSWORD", "root");
+    define("SERVER_NAME","mi3-ss48");
+    define("DBF_USER_NAME", "sportrad_root");
+    define("DBF_PASSWORD", "jeR2N1tgRaZQ");
     define("DATABASE_NAME", "sportrad_sportrader");
 
 	// establish connection
