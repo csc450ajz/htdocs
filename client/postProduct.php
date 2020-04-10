b2338fcac23ff890f3436880fea0aa9a8642c97a
acmaser12 <49576923+acmaser12@users.noreply.github.com>
2020-04-10 11:26:06 -0500

    Updated links and removed unnecesary Jquery

--- client/postProduct.php
+++ client/postProduct.php
@@ -1,12 +1,10 @@
 <?php
 // if user is already logged in, redirect to profile page
-include('../util/check-login.php');
+include('../util/config.php');
 if (!checkLogin()) {
     header('Location: /logIn.php');
 }
 
-// create db connection 
-include('../util/db-config.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // check that a max of 5 files were passed
@@ -45,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <!DOCTYPE html>
 <html>
 <?php
-include('../util/config.php');
+require_once(('../util'.$navbar));
 ?>
 
 <head>
@@ -145,10 +143,6 @@ include('../util/config.php');
             </div>
         </div>
     </div>
-     <!-- jQuery CDN links for Bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script>
         // function prevents more than 5 files from being uploaded
         jQuery('#product-form').submit(function(){
