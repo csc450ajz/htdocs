8937c988f674429ef8a4b272d376cd857a24e710
acmaser12 <49576923+acmaser12@users.noreply.github.com>
2020-04-10 11:26:24 -0500

    Removed unnecessary session_start()

--- client/cart/cart-utility.php
+++ client/cart/cart-utility.php
@@ -18,7 +18,7 @@
     - If logged in, stores productId in CartItems table with session userEmail
     */
     function addCartItem($itemId, $conn) {
-        session_start();
+        //session_start();
         // make sure the user is logged in, otherwise redirect to login page
         if(isset($_SESSION['userEmail'])) {
             // get productId and userEmail
