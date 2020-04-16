a09fffcc87e7a72ebde8bdae4dd90af1d474f02e
zulkiflmohammed <zulkiflmohammed@gmail.com>
2020-04-16 17:46:30 -0500

    Display user detail for editing

--- client/editProfile.php
+++ client/editProfile.php
@@ -7,27 +7,35 @@ if (!checkLogin()) {
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		$userEmail = $_SESSION['userEmail'];
-        // gather form values
-        $fName = $_POST['fName'];
-        $lName = $_POST['lName'];
-        $password = $_POST['password'];
+    $userEmail = $_SESSION['userEmail'];
+    // gather form values
+    $fName = $_POST['fName'];
+    $lName = $_POST['lName'];
+    $password = $_POST['password'];
 
-		//Store Image
-        require_once('../util/image-util.php');
-        $photoPath = storeProfileImage($_FILES['profilePic']);
+    //Store Image
+    require_once('../util/image-util.php');
+    $photoPath = storeProfileImage($_FILES['profilePic']);
 
-        // Update Profile   
-        $sql = "UPDATE User SET userFName = '$fName', userLName = '$lName', userPassword = '$password', userPhotoPath = '$photoPath' WHERE userEmail = '$userEmail'";
-        $result = $conn->query($sql);
+    // Update Profile   
+    $sql = "UPDATE User SET userFName = '$fName', userLName = '$lName', userPassword = '$password', userPhotoPath = '$photoPath' WHERE userEmail = '$userEmail'";
+    $result = $conn->query($sql);
 }
+
+//Get user detail for editing
+$sql = "SELECT * FROM User WHERE userEmail='$userEmail'";
+$result = $conn->query($sql);
+$row = mysqli_fetch_assoc($result);
+
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <?php
-require_once(('../util'.$navbar));
+require_once(('../util' . $navbar));
+
+
 ?>
 
 <head>
@@ -35,7 +43,7 @@ require_once(('../util'.$navbar));
 </head>
 
 <body>
-<div class="container">
+    <div class="container">
         <div class="row justify-content-sm-center">
             <div class="col col-sm-10 col-md-8 col-lg-8">
                 <h1>Edit Profile</h1>
@@ -47,17 +55,17 @@ require_once(('../util'.$navbar));
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label for="fName">First Name</label>
-                            <input type="text" name="fName" class="form-control" id="fName" placeholder = "First Name" maxlength="100" required>
+                            <input type="text" name="fName" class="form-control" id="fName" value="<?= $row['userFName'] ?>" maxlength="100" >
                         </div>
                         <div class="form-group col-md-6">
                             <label for="lName">Last Name</label>
-                            <input type="text" name="lName" class="form-control" id="lName" placeholder = "Last Name" maxlength="100" required>
+                            <input type="text" name="lName" class="form-control" id="lName" value="<?= $row['userLName'] ?>" maxlength="100" >
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label for="password">New Password</label>
-                            <input type="text" name="password" class="form-control" id="password" placeholder="New Password" maxlength="100" required>
+                            <input type="text" name="password" class="form-control" id="password" placeholder="New Password" maxlength="100" >
                         </div>
                     </div>
                     <div class="form-row">
